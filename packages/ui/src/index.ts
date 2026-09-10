8921c5c6bc03195bfb695033741a919baed2d1b2
LIlGG <1103069291@qq.com>
2026-09-10 11:24:44 +0800

    feat: support pinning comments

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,6 +6,18 @@ export default definePlugin({
   components: {},
   routes: [],
   extensionPoints: {
+    'comment:list-item:operation:create': (comment) => [
+      {
+        priority: 25,
+        component: markRaw(
+          defineAsyncComponent(
+            () => import('./components/PinCommentOperation.vue')
+          )
+        ),
+        props: { getComment: () => comment.value.comment },
+        permissions: ['system:comments:manage'],
+      },
+    ],
     'comment:editor:replace': () => {
       return {
         component: markRaw(
